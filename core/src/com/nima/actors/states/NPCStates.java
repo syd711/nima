efd5612857d498510f148227a375c70b3aae728b
syd711 <syd711@googlemail.com>
2017-01-15 18:17:05 +0100

    qa

--- core/src/com/nima/actors/states/NPCStates.java
+++ core/src/com/nima/actors/states/NPCStates.java
@@ -14,4 +14,5 @@ public class NPCStates {
   public static State EVADE_PLAYER = new EvadePlayerState();
   public static State FACE_PLAYER = new FacePlayerState();
   public static State ROUTE_POINT_ARRIVED = new RoutingPointArrivedState();
+  public static State PURSUE_PLAYER = new PursuePlayerState();
 }
