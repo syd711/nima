6d1aee64636d2c513a72e01a55e61d867f652a4c
syd711 <syd711@googlemail.com>
2017-01-30 18:40:31 +0100

    qa

--- core/src/com/starsailor/actors/states/npc/NPCStates.java
+++ core/src/com/starsailor/actors/states/npc/NPCStates.java
@@ -8,8 +8,6 @@ import com.badlogic.gdx.ai.fsm.State;
 public class NPCStates {
 
   public static State IDLE = new IdleState();
-  public static State ROUTE = new RouteState();
-  public static State GUARD = new GuardState();
   public static State EVADE_PLAYER = new EvadePlayerState();
   public static State FACE_PLAYER = new FacePlayerState();
   public static State ROUTE_POINT_ARRIVED = new RoutingPointArrivedState();
